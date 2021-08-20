738265ad08b06a4fb039fc3826b9ec6ac025cb60
THONWELLING <thonwellingdani13@gmail.com>
2021-08-20 19:11:50 -0300

    início do desenvolvimento

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import {Container} from './styles'
+
+
+const Home = () => {
+  return (
+    <Container>HELLO WORLD</Container>
+  );
+}
+ 
+export default Home;
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+    primary: '#6200ee',
+    background: '#000014',
+    text: '#0000b3',
+    success: '#135E01',
+  },
+  fonts: {
+    regular: 'Roboto, sans-serif',
+  }
+};
+
+
+export default theme;
